e5994a7b5fb8166a5ed18ca65e34a6db5392a560
monicanimm <monicanimm@gmail.com>
2017-03-05 16:53:23 +0100

    raindrop drop top

---
+++ Assets/Scripts/obstacle/raindrop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raindrop : MonoBehaviour {
+	public float speed=5;
+	public float lifeTime=5;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (KillAfterSeconds (lifeTime));	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Vector3.down * speed * Time.deltaTime;
+	}
+
+	IEnumerator KillAfterSeconds(float seconds){
+		yield return new WaitForSeconds(seconds); 
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/obstacle/raindropspawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raindropspawn : MonoBehaviour {
+	public float spawnrate=5;
+	public float spawnwidth=5;
+	private float spawntime=0;
+	public GameObject raindropPreFab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (spawntime + 1 / spawnrate < Time.time) {
+			spawntime = Time.time;
+			Vector3 spawnPosition = transform.position;
+			spawnPosition += new Vector3(Random.Range(-spawnwidth, spawnwidth), 0, 0);
+		
+			Instantiate(raindropPreFab, spawnPosition, Quaternion.identity);
+		}
+	}
+}
