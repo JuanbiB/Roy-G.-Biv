db35cbfd17edd4bdc8ded4785134a83a829bc15a
monicanimm <monicanimm@gmail.com>
2017-03-06 10:52:41 +0100

    raindrop spawn, not black mode tho

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -78,7 +78,7 @@ public class Player : MonoBehaviour {
 			Destroy (gameObject);
 		}
 	
-		else if (other.CompareTag("raindrop") && instance.playerMode!=Mode.Black) {
+		 if (other.CompareTag("raindrop") && instance.playerMode!=Mode.Black) {
 			StartCoroutine (WaitSeconds (blacklifetime));
 			instance.playerMode = Mode.White;
 
