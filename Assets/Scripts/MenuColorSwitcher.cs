e2c67df6ad842127ec7460a58a281dfd33e0f1d1
JuanbiB <juanbiiberretta@gmail.com>
2017-03-09 10:26:25 +0100

    ch-ch-ch-cho-choo!

---
+++ Assets/Scripts/MenuColorSwitcher.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuColorSwitcher : MonoBehaviour {
+
+	Text royText;
+	float timeCounter;
+	Color [] colors;
+	int counter;
+
+	// Use this for initialization
+	void Start () {
+		royText = GetComponent<Text> ();
+		timeCounter = 0;
+		royText.color = Color.red;
+
+		colors = new Color[3]{Color.red, Color.yellow, Color.blue};
+		counter = 0;
+	}
+	
+	void Update(){
+		timeCounter += Time.deltaTime;
+
+		if (timeCounter > .5f) {
+			changeColor ();
+			timeCounter = 0;
+		}
+	}
+
+	void changeColor(){
+		counter++;
+		Color newColor = colors [counter % 3];
+		royText.color = newColor;
+	}
+}
